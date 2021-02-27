d6e8069402559663fcb24dbbbf59c0e73dfcff95
gavin <gavin@gmail.com>
2021-02-27 12:32:25 +0800

    remove hello there

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hello: 'there' });
-});
+// app.get('/', (req, res) => {
+//   res.send({ hello: 'there' });
+// });
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
